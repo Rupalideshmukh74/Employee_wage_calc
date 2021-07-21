8df7a6ef1afee887422669b546e48f8f963c07b9
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-07-22 00:12:19 +0530

    [Rupali]Refractor:switch case base calculation implemented

--- EmployeeWageCalculation/EmpCheck.cs
+++ EmployeeWageCalculation/EmpCheck.cs
@@ -7,7 +7,7 @@ namespace EmployeeWageCalculation
     class EmpCheck
     {
         
-       /* public void AtendanceCheck(int empPresent)
+        public void AtendanceCheck(int empPresent)
         {
             if (empPresent == 1)
             {
@@ -19,13 +19,31 @@ namespace EmployeeWageCalculation
             }
 
         }
-       */
-       public void CalculateDailyWage()
+       
+       public void CalculateDailyWage(int emPresent)
         {
             //int FullTime = 8;
-            int PartTime = 4;
+          //  int PartTime = 4;
             int WagePerHour = 20;
-            int DailyPartWage = PartTime * WagePerHour;
+            int WorkHours = 0;
+            switch(emPresent)
+            {
+                case 1:
+                    Console.WriteLine("Employee  present part time ");
+                    WorkHours = 4;
+                    break;
+                case 2:
+                    Console.WriteLine("Employee  present full time ");
+                    WorkHours = 8;
+                    break;
+                default:
+                    Console.WriteLine("Employee absent ");
+                    break;
+                    
+
+            }
+            Console.WriteLine("Calculating Emp Wage");
+            int DailyPartWage = WagePerHour * WorkHours;
             Console.WriteLine(" DailyPartWage : " + DailyPartWage);
 
         }
