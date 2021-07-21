0719cd5529716e33d5c52bec72412d7331178c92
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-07-21 22:11:49 +0530

    [Rupali] Add:welcome msg added

---
+++ EmployeeWageCalculation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee wage calculation program!");
+        }
+    }
+}
